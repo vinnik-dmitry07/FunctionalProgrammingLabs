bcdd30a49a73e829a52efe2256839089b798b034
Dmitry Vinnik <vinnik.dmitry07@gmail.com>
2020-04-01 01:59:35 +0300

    Update lab1_1.pl

--- lab1_1.pl
+++ lab1_1.pl
@@ -1,6 +1,5 @@
 list_min([L|Ls], Min) :-
     list_min(Ls, L, Min).
-
 list_min([], Min, Min).
 list_min([L|Ls], Min0, Min) :-
     Min1 is min(L, Min0),
@@ -11,4 +10,4 @@ index_of([_|Tail], Element, Index):-
   index_of(Tail, Element, Index1),
   Index is Index1+1.
 
-?- List = [5, 2, 3, 2], list_min(List, Min), index_of(List, Min, MinIndex), write(MinIndex)
+?- List = [5, 2, 3, 2], list_min(List, Min), index_of(List, Min, MinIndex), write(MinIndex).
