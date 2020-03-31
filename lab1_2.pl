45b45ba6f00f1f1cb767bfe4d9becce42eff6abe
Dmitry Vinnik <vinnik.dmitry07@gmail.com>
2020-04-01 01:59:09 +0300

    Update lab1_2.pl

--- lab1_2.pl
+++ lab1_2.pl
@@ -1,18 +1,16 @@
 remove_duplicates([], []).
-
 remove_duplicates([H | T], List) :-    
      member(H, T),
      remove_duplicates(T, List).
-
 remove_duplicates([H | T], [H | T1]) :- 
       \+member(H, T),
       remove_duplicates(T, T1).
 
 count([], _, 0).
-
 count([X | T], X, Y):- !, count(T, X, Z), Y is 1 + Z.
-
 count([_|T], X, Z):- count(T, X, Z).
 
 get_entries([], _).
 get_entries([H | T], L) :- count(L, H, R), write(H), write(" - "), write(R), write("; "), get_entries(T, L).
+
+?- List = [5, 2, 3, 2], remove_duplicates(List, List1), sort(List1, List1Sorted), get_entries(List1Sorted, List).
