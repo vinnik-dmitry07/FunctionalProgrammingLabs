0904d7daf63bf1e62102f6681accdf205d8ad723
Dmitry Vinnik <vinnik.dmitry07@gmail.com>
2020-04-29 13:15:27 +0300

    Update lab2.hs

--- lab2.hs
+++ lab2.hs
@@ -1,8 +1,12 @@
 splitByFactorial :: [Integer] -> [[Integer]]
-splitByFactorial x = splitByFactorial' x (maximum x) 1
+splitByFactorial list = splitByFactorial' list (maximum list) 2 1
 
-splitByFactorial' :: [Integer] -> Integer -> Integer -> [[Integer]]
-splitByFactorial' x upperest_bound lower_bound = [x_i | x_i <- x, lower_bound <= x_i && x_i < upper_bound] : (if upper_bound < upperest_bound then (splitByFactorial' x upperest_bound upper_bound) else [])
-  where upper_bound = lower_bound * (lower_bound + 1)
+splitByFactorial' :: [Integer] -> Integer -> Integer -> Integer -> [[Integer]]
+splitByFactorial' list upperest_bound i lower_bound = 
+    [l |  l <- list, lower_bound <= l && l < upper_bound] : 
+            (if upper_bound < upperest_bound 
+                then (splitByFactorial' list upperest_bound (i + 1) upper_bound) 
+                else [])
+        where upper_bound = lower_bound * i
 
 main = print $ splitByFactorial [1, 3, 5, 2, 6, 7, 12, 14, 9]
