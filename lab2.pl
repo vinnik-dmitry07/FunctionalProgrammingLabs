b27ecf7dd066b3c4fc04d01322adba9669592eff
Dmitry Vinnik <vinnik.dmitry07@gmail.com>
2020-04-29 14:22:37 +0300

    Update lab2.pl

--- lab2.pl
+++ lab2.pl
@@ -4,11 +4,11 @@ get_peaks(List, Res) :-
 
 get_peaks([_, _], [], _).
 get_peaks([L1, L2, L3 | LS], [(L2, Pos)| R], Pos) :-
-    L2>L1, L2>L3, 
+    L2 > L1, L2 > L3, 
     Pos1 is Pos + 1,
     get_peaks([L2, L3 | LS], R, Pos1), !.
 get_peaks([_, L2, L3 | LS], R, Pos) :- 
     Pos1 is Pos + 1, 
     get_peaks([L2, L3 | LS], R, Pos1).
 
-?- List=[5,4,2,8,3,1,6,9,5], get_peaks(List, Res), write(Res).
+?- List=[5, 4, 2, 8, 3, 1, 6, 9, 5], get_peaks(List, Res), write(Res).
